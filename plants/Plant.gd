6ec1221616a68fef0295fe000a8a6fe2a9a40b88
tickyyyy <lrtanner@ucsc.edu>
2024-12-07 13:32:36 -0800

    f2 fixed sun and water req issue in decoding
    Sun and water reqs were not being set in plant.gd anymore, added functions to assign requirements when initialized

--- plants/Plant.gd
+++ plants/Plant.gd
@@ -14,6 +14,9 @@ enum GrowthStage { STAGE_0, STAGE_1, STAGE_2 }
 var growth_level = GrowthStage.STAGE_0
 var max_growth_level = GrowthStage.STAGE_2  # Final growth stage
 
+var sun_req = 0
+var water_req = 0
+
 # The plot this plant is currently on (reference to Plot node)
 var current_plot = null  # This will be set when the plant is placed
 
@@ -33,33 +36,53 @@ class GrowthRule:
 # Specific growth rules for each plant type
 class LettuceGrowthRule extends GrowthRule:
 	func can_grow(plant, plot) -> bool:
-		return plot.get_adjacent_plots().any(
-			func(adjacent) -> bool:
-				return adjacent.has_plant() and adjacent.get_plant().type == PlantType.LETTUCE
-		)
+		if (plot.sun_level >= plant.sun_req) and (plot.water_level >= plant.water_req):
+			return plot.get_adjacent_plots().any(
+				func(adjacent) -> bool:
+					return adjacent.has_plant() and adjacent.get_plant().type == PlantType.LETTUCE
+			)
+		return false
 
 class TomatoGrowthRule extends GrowthRule:
 	func can_grow(plant, plot) -> bool:
-		return not plot.get_adjacent_plots().any(
-			func(adjacent) -> bool:
-				return adjacent.has_plant()
-		)
+		if (plot.sun_level >= plant.sun_req) and (plot.water_level >= plant.water_req):
+			return not plot.get_adjacent_plots().any(
+				func(adjacent) -> bool:
+					return adjacent.has_plant()
+			)
+		return false
 
 class CarrotGrowthRule extends GrowthRule:
 	func can_grow(plant, plot) -> bool:
-		return not plot.get_adjacent_plots().any(
-			func(adjacent) -> bool:
-				return adjacent.has_plant() and adjacent.get_plant().type == PlantType.CARROT
-		)
+		if (plot.sun_level >= plant.sun_req) and (plot.water_level >= plant.water_req):
+			return not plot.get_adjacent_plots().any(
+				func(adjacent) -> bool:
+					return adjacent.has_plant() and adjacent.get_plant().type == PlantType.CARROT
+			)
+		return false
 
 # Initialization function, called when the node is ready
 func _ready():
+	assign_sun_water_reqs()
 	# Assign the growth rule based on the plant type
 	assign_growth_rule()
 
 	# Update visuals based on the current growth level
 	update_visuals()
 
+# Assign the sun and water requirements based on the plant type
+func assign_sun_water_reqs():
+	var plant_sun_water_reqs = {
+		PlantType.LETTUCE: { "sun": 5, "water": 3 },
+		PlantType.TOMATO: { "sun": 6, "water": 4 },
+		PlantType.CARROT: { "sun": 4, "water": 2 }
+	}
+	# Assign the requirements to the plant
+	var reqs = plant_sun_water_reqs.get(type)
+	if reqs:
+		sun_req = reqs["sun"]
+		water_req = reqs["water"]
+		
 # Assign the appropriate growth rule based on the plant type
 func assign_growth_rule():
 	var plant_growth_rules = {
